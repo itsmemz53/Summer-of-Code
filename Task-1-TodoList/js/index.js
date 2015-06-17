57f0990575545fd9f545f939f36af71790dac187
Mohammad <itsmemz52@gmail.com>
2015-06-17 12:55:52 +0500

    update

--- Task-1-TodoList/js/index.js
+++ Task-1-TodoList/js/index.js
@@ -2,34 +2,7 @@ var arr=[];
 var storage=[];
 var value = $("#txtTodo").val();
 $(function() {
-	$.get('http://datastore.asadmemon.com/mz',function(res)
-{
-		arr=res;
-		storage=arr;
-		for (var i = 0; i < storage.length ;i++) 
-		{
-				var show=$('<div class="todo-holder" style="border-radius: 8px;border: 2px solid #8AC007;margin: 5px;padding: 1px; width: 70%;height: 30px; "><label><input type="checkbox" class="todo-checkbox" /> <span class="todo">'+storage[i].text+'</label></div>');
-				show.attr("data-id",i);
-				if(storage[i].state==true)
-				{
-					$(show).find(".todo").css("text-decoration", "line-through");
-					$(show).find(".todo-checkbox").prop("checked",true);
-				}
-				$("#pending-todos").append(show);
-		};
-});
-	
-	function saveit(){
-		console.log(arr);
-		$.ajax({
-	    url: 'http://datastore.asadmemon.com/mz', 
-	    type: 'POST', 
-	    contentType: 'application/json', 
-	    data: JSON.stringify(arr),
-	success:function(res){console.log(res);}
-	});
-	};
-	
+		
 	$("#btnAddTodo").click(function(e)
 	 {
 		value = $("#txtTodo").val();
